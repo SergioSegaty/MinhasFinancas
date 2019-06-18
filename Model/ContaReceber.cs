4f5ec486726040086bf29ec308014c2763e79393
Sergio Segaty <sergio.segaty@gmail.com>
2019-06-18 16:59:16 -0300

    Criação do Modelo ContaReceber

---
+++ Model/ContaReceber.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class ContaReceber
+    {
+        public int Id;
+        public decimal Valor;
+        public string Nome;
+        public string Tipo;
+        public string Descricao;
+        public string Status;
+    }
+}
